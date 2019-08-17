c7b771242e76240d32e4a13d242afcd22482eddf
Fábio Pereira <fabiopereira.fps@gmail.com>
2019-08-17 02:40:24 -0300

    Add images processing and clean build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const config = {
     mode: 'development',
@@ -10,16 +11,60 @@ const config = {
         filename: '[name].js',
         publicPath: '/'
     },
-    devtool: 'source-map',
     module: {
         rules: [
             {
                 test: /\.hbs$/,
-                loader: 'handlebars-loader'
+                use: {
+                    loader: 'handlebars-loader',
+                    options: { inlineRequires: '/assets/img' }
+                }
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name(file) {
+                                if (process.env.NODE_ENV !== 'production') {
+                                    return 'assets/img/[name].[ext]';
+                                }
+
+                                return 'assets/img/[contenthash].[ext]';
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false
+                            },
+                            webp: {
+                                quality: 75
+                            },
+                            svgo: {
+                                removeTitle: true,
+                                convertColors: { shorthex: false },
+                                convertPathData: false
+                            }
+                        }
+                    }
+                ]
             }
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'Static Website Boilerplate',
             template: path.resolve(
@@ -38,6 +83,7 @@ const config = {
             Scripts: path.resolve(__dirname, 'src/scripts/')
         }
     },
+    devtool: 'source-map',
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
