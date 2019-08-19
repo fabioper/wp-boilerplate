0c46b7d92e685c8c6203114aeee68e3ec1215321
Fábio Pereira <fabiopereira.fps@gmail.com>
2019-08-19 02:40:28 -0300

    Add config file and move .default-imports.js inside webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,36 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable id-length */
 const path = require('path');
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const appConfig = require('./config.json');
+const templatesPath = path.resolve(__dirname, 'src', 'templates', 'pages');
 
-const changeExtensionToHtml = filename => filename.replace(/\.(.*)/, '.html');
+const templatesPages = fs.readdirSync(
+    path.resolve(__dirname, 'src', 'templates', 'pages'),
+    'utf-8'
+);
 
-const generateTemplates = templates => {
-    const templatesPath = path.resolve(__dirname, 'src', 'templates', 'pages');
-
-    return templates.map(templateOptions => {
-        return new HtmlWebpackPlugin({
-            ...templateOptions,
-            filename: changeExtensionToHtml(templateOptions.template),
-            template: path.resolve(templatesPath, templateOptions.template)
-        });
-    });
-};
+const generateTemplates = templates =>
+    templates.map(
+        template =>
+            new HtmlWebpackPlugin({
+                filename: template.replace(/\.(.*)/, '.html'),
+                template: path.resolve(templatesPath, template),
+                meta: {
+                    description: appConfig.description,
+                    author: appConfig.author
+                }
+            })
+    );
 
 const config = {
-    mode: 'development',
+    mode: 'production',
     target: 'web',
     entry: path.resolve(__dirname, 'src', 'scripts', 'app.js'),
     output: {
@@ -134,20 +143,26 @@ const config = {
             chunkFilename: '[id].css',
             ignoreOrder: false
         }),
-        ...generateTemplates(require('./.default-imports')),
+        ...generateTemplates(templatesPages),
         new FaviconsWebpackPlugin({
-            logo: path.resolve(
-                __dirname,
-                'src',
-                'assets',
-                'images',
-                'logo.svg'
-            ),
-            devMode: 'webapp'
+            devMode: 'webapp',
+            logo: path.resolve(__dirname, appConfig.logo),
+            favicons: {
+                appName: appConfig.name,
+                appDescription: appConfig.description,
+                background: appConfig.background,
+                theme_color: appConfig.theme_color
+            }
         })
     ],
     resolve: {
-        extensions: ['.wasm', '.mjs', '.js', '.jsx', '.json'],
+        extensions: [
+            '.wasm',
+            '.mjs',
+            '.js',
+            '.jsx',
+            '.json'
+        ],
         alias: {
             assets: path.resolve(__dirname, 'src/assets/'),
             styles: path.resolve(__dirname, 'src/styles/'),
