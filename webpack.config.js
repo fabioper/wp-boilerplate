4212e82e4ccacf70be27ba810fa73d8a7e3ddf00
Fábio Pereira <fabiopereira.fps@gmail.com>
2019-09-26 16:58:20 -0300

    Fix ghost bug

--- webpack.config.js
+++ webpack.config.js
@@ -281,14 +281,14 @@ if (appConfig.wordpress) {
     config.devServer.publicPath = `http://localhost/${path.basename(path.join(__dirname, '..'))}/`
     config.output.path = path.resolve(__dirname, '..', 'wp-content', 'themes', slug(appConfig.theme.name, { lower: true }))
     config.output.publicPath = path.join('wp-content', 'themes', slug(appConfig.theme.name, { lower: true }))
-    config.devServer.publicPath = '/'
+    // config.devServer.publicPath = '/'
     config.output.filename = 'scripts/[name].js'
-    config.devServer.writeToDisk = true
+    // config.devServer.contentBase = config.output.path;
     config.devServer.contentBase = config.output.path
     config.devServer.proxy = {
         '/': {
             target: `http://localhost/${path.basename(path.join(__dirname, '..'))}/`,
-            changeOrigin: true
+            changeOrigin: false
         }
     }
 }
